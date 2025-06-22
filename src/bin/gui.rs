ccae09e43218cbe1ba1a05cf0a6b6d4aa2634099
Andrzej Fricze <fricza01@heiway.net>
2025-06-22 11:05:00 +0200

    Add file dialog to pick file

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -3,9 +3,14 @@
 use clap::Parser;
 use csv::StringRecord;
 use egui::Key;
+use egui_file::FileDialog;
 use std::{
     collections::HashSet,
     ops::{Add, Sub},
+};
+use std::{
+    ffi::OsStr,
+    path::{Path, PathBuf},
 };
 
 use egui_extras::{Column, TableBuilder};
@@ -38,18 +43,24 @@ fn main() -> eframe::Result {
 
     let app = MyApp {
         filename: args.file,
-        headers: headers
-            .into_iter()
-            .map(|name| FileHeader {
-                name: name.to_string(),
-                visible: true,
-            })
-            .collect(),
-        data,
+        // headers: Some(
+        //     headers
+        //         .into_iter()
+        //         .map(|name| FileHeader {
+        //             name: name.to_string(),
+        //             visible: true,
+        //         })
+        //         .collect(),
+        // ),
+        // data: Some(data),
+        headers: None,
+        data: None,
         scroll_y: 0.0,
         inner_rect: 0.0,
         content_height: 0.0,
         filter: "".to_owned(),
+        opened_file: None,
+        open_file_dialog: None,
     };
 
     eframe::run_native(
@@ -64,44 +75,73 @@ struct FileHeader {
     visible: bool,
 }
 
+#[derive(Default)]
 struct MyApp {
     filename: String,
-    headers: Vec<FileHeader>,
-    data: Vec<StringRecord>,
+    headers: Option<Vec<FileHeader>>,
+    data: Option<Vec<StringRecord>>,
     scroll_y: f32,
     inner_rect: f32,
     content_height: f32,
     filter: String,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            filename: "data.csv".to_owned(),
-            headers: Vec::new(),
-            data: Vec::new(),
-            scroll_y: 0.0,
-            inner_rect: 0.0,
-            content_height: 0.0,
-            filter: "".to_owned(),
-        }
-    }
+    opened_file: Option<PathBuf>,
+    open_file_dialog: Option<FileDialog>,
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(format!(
-                "CSV reader :: {}, {}",
-                self.filename, self.scroll_y
-            ));
+            ui.label(format!("CSV reader :: {}", self.filename));
 
-            ui.horizontal(|ui| {
-                for file_header in self.headers.iter_mut() {
-                    ui.checkbox(&mut file_header.visible, &file_header.name)
-                        .on_hover_text(format!("Show/hide column {}", file_header.name));
+            if let Some(headers) = self.headers.as_mut() {
+                ui.horizontal(|ui| {
+                    for file_header in headers.iter_mut() {
+                        ui.checkbox(&mut file_header.visible, &file_header.name)
+                            .on_hover_text(format!("Show/hide column {}", file_header.name));
+                    }
+                });
+            }
+
+            if (ui.button("Open")).clicked() {
+                let filter = Box::new({
+                    let ext = Some(OsStr::new("csv"));
+                    move |path: &Path| -> bool { path.extension() == ext }
+                });
+                let mut dialog =
+                    FileDialog::open_file(self.opened_file.clone()).show_files_filter(filter);
+                dialog.open();
+                self.open_file_dialog = Some(dialog);
+            }
+
+            if let Some(dialog) = &mut self.open_file_dialog {
+                if dialog.show(ctx).selected() {
+                    if let Some(file) = dialog.path() {
+                        self.opened_file = Some(file.to_path_buf());
+
+                        let path = file.to_path_buf();
+                        let read_path = path.to_str().unwrap_or("");
+
+                        match read_csv::read_csv(&read_path) {
+                            Ok((data, headers)) => {
+                                self.data = Some(data);
+                                self.headers = Some(
+                                    headers
+                                        .into_iter()
+                                        .map(|name| FileHeader {
+                                            name: name.to_string(),
+                                            visible: true,
+                                        })
+                                        .collect(),
+                                );
+                            }
+                            Err(err) => {
+                                eprintln!("Error reading CSV file: {}", err);
+                                std::process::exit(1);
+                            }
+                        };
+                    }
                 }
-            });
+            }
 
             ui.separator();
 
@@ -114,16 +154,6 @@ impl eframe::App for MyApp {
                 .resizable(true)
                 .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
                 .min_scrolled_height(0.0);
-
-            let mut hidden = HashSet::new();
-
-            for (index, file_header) in self.headers.iter().enumerate() {
-                if file_header.visible {
-                    table = table.column(Column::auto());
-                } else {
-                    hidden.insert(index);
-                }
-            }
 
             if ctx.input(|i| i.key_pressed(Key::PageUp)) {
                 table =
@@ -142,44 +172,57 @@ impl eframe::App for MyApp {
                 table = table.vertical_scroll_offset(self.content_height);
             }
 
-            let table_ui = table.header(20.0, |mut header| {
-                for file_header in self.headers.iter().filter(|header| header.visible) {
-                    header.col(|ui| {
-                        ui.heading(&file_header.name);
-                    });
-                }
-            });
+            if let Some(headers) = self.headers.as_ref() {
+                let mut hidden = HashSet::new();
 
-            let scroll_area = table_ui.body(|mut body| {
-                let rows = &self.data;
-                let filtered_rows = rows.iter().filter(|row| {
-                    row.iter()
-                        .find(|text| text.contains(&self.filter))
-                        .is_some()
+                for (index, file_header) in headers.iter().enumerate() {
+                    if file_header.visible {
+                        table = table.column(Column::auto());
+                    } else {
+                        hidden.insert(index);
+                    }
+                }
+
+                let table_ui = table.header(20.0, |mut header| {
+                    for file_header in headers.iter().filter(|header| header.visible) {
+                        header.col(|ui| {
+                            ui.heading(&file_header.name);
+                        });
+                    }
                 });
 
-                for row_data in filtered_rows {
-                    body.row(20.0, |mut row| {
-                        row_data
-                            .iter()
-                            .enumerate()
-                            .filter(|(index, _)| !hidden.contains(index))
-                            .for_each(|(_, data)| {
-                                row.col(|ui| {
-                                    ui.label(data);
-                                });
+                if let Some(data) = self.data.as_ref() {
+                    let scroll_area = table_ui.body(|mut body| {
+                        let filtered_rows = data.iter().filter(|row| {
+                            row.iter()
+                                .find(|text| text.contains(&self.filter))
+                                .is_some()
+                        });
+
+                        for row_data in filtered_rows {
+                            body.row(20.0, |mut row| {
+                                row_data
+                                    .iter()
+                                    .enumerate()
+                                    .filter(|(index, _)| !hidden.contains(index))
+                                    .for_each(|(_, data)| {
+                                        row.col(|ui| {
+                                            ui.label(data);
+                                        });
+                                    });
                             });
+                        }
                     });
+
+                    let content_height = scroll_area.content_size[1];
+
+                    self.content_height = content_height;
+
+                    let offset = scroll_area.state.offset[1];
+                    self.scroll_y = offset;
+                    self.inner_rect = scroll_area.inner_rect.height();
                 }
-            });
-
-            let content_height = scroll_area.content_size[1];
-
-            self.content_height = content_height;
-
-            let offset = scroll_area.state.offset[1];
-            self.scroll_y = offset;
-            self.inner_rect = scroll_area.inner_rect.height();
+            }
         });
     }
 }
